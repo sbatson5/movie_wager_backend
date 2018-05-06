d37faa8d7d17d3fade5eaaa5b411698eb355dbfe
Scott Batson <sjbats@email.com>
2018-05-06 14:28:41 -0400

    prevent wagers after end_date

--- lib/movie_wager_backend_web/controllers/wager_controller.ex
+++ lib/movie_wager_backend_web/controllers/wager_controller.ex
@@ -22,12 +22,17 @@ defmodule MovieWagerBackendWeb.WagerController do
   end
 
   def create(conn, %{"data" => %{"attributes" => wager_params, "relationships" => relationship_params}}) do
-    with {:ok, %Wager{} = wager} <- Movie.create_wager(wager_params, relationship_params) do
-      wager = Movie.preload_wager_relationships(wager)
-      conn
-      |> put_status(:created)
-      |> put_resp_header("location", wager_path(conn, :show, wager))
-      |> render("show.json-api", data: wager)
+    case Movie.create_wager(wager_params, relationship_params) do
+      {:ok, %Wager{} = wager} ->
+        wager = Movie.preload_wager_relationships(wager)
+        conn
+        |> put_status(:created)
+        |> put_resp_header("location", wager_path(conn, :show, wager))
+        |> render("show.json-api", data: wager)
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(:errors, data: changeset)
     end
   end
 
@@ -39,9 +44,14 @@ defmodule MovieWagerBackendWeb.WagerController do
   def update(conn, %{"id" => id, "data" => %{"attributes" => wager_params, "relationships" => relationship_params}}) do
     wager = Movie.get_wager!(id)
 
-    with {:ok, %Wager{} = wager} <- Movie.update_wager(wager, wager_params, relationship_params) do
-      wager = Movie.preload_wager_relationships(wager)
-      render(conn, "show.json-api", data: wager)
+    case Movie.update_wager(wager, wager_params, relationship_params) do
+      {:ok, %Wager{} = wager} ->
+        wager = Movie.preload_wager_relationships(wager)
+        render(conn, "show.json-api", data: wager)
+      {:error, changeset} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> render(:errors, data: changeset)
     end
   end
 
