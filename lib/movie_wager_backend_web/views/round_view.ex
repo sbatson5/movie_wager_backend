0c2cc711e9516ca3d585a4fa66a3a35ed1b53d48
Scott Batson <sjbats@email.com>
2018-05-18 15:46:10 -0400

    serialize round fields

--- lib/movie_wager_backend_web/views/round_view.ex
+++ lib/movie_wager_backend_web/views/round_view.ex
@@ -2,5 +2,14 @@ defmodule MovieWagerBackendWeb.RoundView do
   use MovieWagerBackendWeb, :view
   use JaSerializer.PhoenixView
 
-  attributes [:code, :start_date, :end_date, :box_office_amount, :title]
+  attributes [
+    :code,
+    :start_date,
+    :end_date,
+    :box_office_amount,
+    :title,
+    :poster,
+    :plot,
+    :website
+  ]
 end
