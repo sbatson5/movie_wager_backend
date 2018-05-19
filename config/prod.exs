ca8a6242ba0f8e9096fe3f1135b2efaffada8c07
Scott Batson <sjbats@email.com>
2018-05-19 10:34:46 -0400

    fix endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -20,7 +20,7 @@ config :movie_wager_backend, MovieWagerBackendWeb.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config :movie_wager_backend, MovieWagerBackendWeb.Repo,
+config :movie_wager_backend, MovieWagerBackend.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
