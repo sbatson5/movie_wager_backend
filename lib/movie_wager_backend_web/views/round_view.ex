5c256ff4f74a2c1eccba773fae1c60877692c48e
Scott Batson <sjbats@email.com>
2018-07-01 23:41:02 -0400

    add wager info to round serializer

--- lib/movie_wager_backend_web/views/round_view.ex
+++ lib/movie_wager_backend_web/views/round_view.ex
@@ -2,6 +2,8 @@ defmodule MovieWagerBackendWeb.RoundView do
   use MovieWagerBackendWeb, :view
   use JaSerializer.PhoenixView
 
+  import MovieWagerBackend.Movie
+
   attributes [
     :code,
     :start_date,
@@ -10,6 +12,27 @@ defmodule MovieWagerBackendWeb.RoundView do
     :title,
     :poster,
     :plot,
-    :website
+    :website,
+    :highest_wager,
+    :lowest_wager,
+    :number_of_wagers
   ]
+
+  def highest_wager(%{id: round_id}, _conn) do
+    case MovieWagerBackend.Movie.get_highest_wager_by_round(round_id) do
+      nil -> nil
+      wager -> wager.amount
+    end
+  end
+
+  def lowest_wager(%{id: round_id}, _conn) do
+    case MovieWagerBackend.Movie.get_lowest_wager_by_round(round_id) do
+      nil -> nil
+      wager -> wager.amount
+    end
+  end
+
+  def number_of_wagers(%{id: round_id}, _conn) do
+    get_number_of_wagers_by_round(round_id)
+  end
 end
